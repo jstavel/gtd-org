1eced447f1eb169ad242203f3ebc541faff6bd1e
Jan Stavel <stavel.jan@gmail.com>
2026-07-04 22:05:21 +0200

    chore(plaud-downloader): skeleton for tests

---
+++ components/plaud-downloader/test/jstavel/plaud_downloader/core_test.cljs
@@ -0,0 +1,7 @@
+(ns jstavel.plaud-downloader.core-test
+  (:require [jstavel.plaud-downloader.core :as sut]
+            [cljs.test :as t :include-macros true]))
+
+(t/deftest parse-page
+  (t/is (= 1 1))
+  )
